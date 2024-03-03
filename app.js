5b1c1869d09638ace0ad49a9674574dbf41e0e26
hatim <hatim1300@gmail.com>
2024-03-03 23:38:05 +0400

    ...

--- app.js
+++ app.js
@@ -20,6 +20,7 @@ app.post('/api/v2/tours', (req, res) => {
   NewId = tours[tours.length - 1].id + 1;
   newTour = Object.assign({ id: NewId }, req.body);
   tours.push(newTour);
+
   fs.writeFile(
     `${__dirname}/dev-data/data/tours-simple.json`,
     JSON.stringify(tours),
@@ -32,6 +33,22 @@ app.post('/api/v2/tours', (req, res) => {
       });
     }
   );
+});
+
+app.get('/api/v2/tours/:id', (req, res) => {
+  const id = +req.params.id;
+  const tour = tours.find((el) => el.id === id);
+  if (!tour) {
+    res.status(404).json({
+      status: 'fail',
+      data: 'ivalid id',
+    });
+  }
+  res.status(200).json({
+    status: 'success',
+
+    data: { tour: tour },
+  });
 });
 
 const port = 3000;
