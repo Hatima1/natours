c6d88c56929eaaef0b8bd8acc1459ff83d1b11e8
hatim <hatim1300@gmail.com>
2024-11-15 10:19:16 +0400

    favicon update

--- server.js
+++ server.js
@@ -2,8 +2,8 @@ const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 dotenv.config({ path: './config.env' });
 const app = require('./app');
-console.log(process.env);
-console.log(process.env.DATABASE);
+// console.log(process.env);
+// console.log(process.env.DATABASE);
 const DB = process.env.DATABASE.replace(
   '<PASSWORD>',
   process.env.DATABASE_PASSWORD
