9a6d2feae21ee7d14c77144b025601dac090b345
hatim <hatim1300@gmail.com>
2024-11-15 11:47:28 +0400

    home route

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 dotenv.config({ path: './config.env' });
 const app = require('./app');
-// console.log(process.env);
+console.log(process.env.DATABASE_PASSWORD);
 // console.log(process.env.DATABASE);
 const DB = process.env.DATABASE.replace(
   '<PASSWORD>',
